cd693f5f380ca98868da7190cb2dbe6354c4918f
Andrey Kozeletsky <andrey.kozeletsky@gmail.com>
2014-03-16 12:31:20 +0300

    [+] Initial release

--- pnstest/PNSAppDelegate.m
+++ pnstest/PNSAppDelegate.m
@@ -10,9 +10,32 @@
 
 @implementation PNSAppDelegate
 
+static NSString *_deviceTokenKey = @"deviceToken";
+
+#pragma mark - Static methods
+
++ (NSString*)deviceToken
+{
+    return [[NSUserDefaults standardUserDefaults] stringForKey:_deviceTokenKey];
+}
+
++ (void)setDeviceToken:(NSData*)tokenData
+{
+    NSCharacterSet *charactersToRemove = [NSCharacterSet characterSetWithCharactersInString:@"<> "];
+    NSString *result = [[[NSString stringWithFormat:@"%@",tokenData] componentsSeparatedByCharactersInSet:charactersToRemove] componentsJoinedByString: @""];
+    
+    [[NSUserDefaults standardUserDefaults] setValue:result forKey:_deviceTokenKey];
+}
+
+
+#pragma mark -
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    
+    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound)];
+    
     return YES;
 }
 							
@@ -41,6 +64,32 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+#pragma mark - APNS
+
+- (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    [[self class] setDeviceToken:deviceToken];
+    NSLog(@"deviceToken: %@", [[self class] deviceToken]);
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    // TODO: Implementation
+    NSLog(@"error: %@", error);
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    // TODO: Implementation
+    NSLog(@"userInfo: %@", userInfo);
+}
+
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
+{
+    // TODO: Implementation
+    NSLog(@"notification: %@", notification);
 }
 
 @end
